3d85730158ecb7b4ce335178108cb4c0a14df877
Alejandro <eadelgadom@gmail.com>
2022-10-09 00:22:37 +0000

    con los modelos funcionando

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,79 @@
+from .db import db
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'user'
+    # Here we define columns for the table user
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return '<User %r>' % self.first_name
+
+    def serialize(self):                                          #defino el dictionary de mi clase para que le asigne las propiedades a mi objeto
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+            #"password": self.password, do not serialize the password, its a security breach
+        }
+        
+
+class People_Favorites(db.Model):
+    #estoy creando una clase People  
+    __tablename__ = 'people'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)                                        #este tipo de dato me permite escoger en tre imagen video o galeria
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True,unique=False)#esta es la unica llave foranea obligatoria necesaria
+    usuario = db.relationship(User, lazy=True, backref="people") #relacion de uno a muchos un usuario tiene muchos favoritos 
+    name = db.Column(db.String(250), unique=False, nullable=True) #este dato lo recibo de mi starwars api
+    picture_url = db.Column(db.String(250), unique=False, nullable=True) #este dato lo recibo de mi starwars api
+    description = db.Column(db.String(250), unique=False, nullable=True) #este dato lo recibo de mi starwars api
+    
+
+    def __repr__(self):
+        return '<PeopleFavorites %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "usuario": self.user_id,
+            "name": self.name,
+            "picture_url": self.picture_url,
+            "description": self.description          
+        }
+
+class Planets_Favorites(db.Model):
+    #estoy creando una clase PLANETAs media que hereda el ID de mi tabla Post
+    __tablename__ = 'planetas'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True,unique=False)#esta es la unica llave foranea obligatoria necesaria
+    usuario = db.relationship(User, lazy=True, backref="planetas") #relacion de uno a muchos un usuario tiene muchos favoritos                                        #este tipo de dato me permite escoger en tre imagen video o galeria
+    planetas_name = db.Column(db.String(250)) #este dato lo recibo de mi starwars api
+    planetas_picture_url = db.Column(db.String(250)) #este dato lo recibo de mi starwars api
+    planetas_description = db.Column(db.String(250)) #este dato lo recibo de mi starwars api
+
+    
+    def __repr__(self):
+        return '<Planets_Favorites %r>' % self.id
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "usuario": self.user_id,
+            "name": self.planetas_name,
+            "pictura_url": self.planetas_picture_url,
+            "description":self.planetas_description       
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,11 +2,17 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from .db import db
+from api.models import db, User, Planets_Favorites, People_Favorites
 from api.utils import generate_sitemap, APIException
+from flask_bcrypt import Bcrypt 
+#from flask_jwt_extended import JWTManager, create_access_token,create_refresh_token, jwt_required, get_jwt_identity,get_jwt
+import datetime 
+import tempfile
 
+app = Flask(__name__)
 api = Blueprint('api', __name__)
-
+bcrypt = Bcrypt(app)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
@@ -15,4 +21,129 @@ def handle_hello():
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
 
+    return jsonify(response_body), 200
+
+#ruta para crear el usuario
+@app.route('/signup', methods=['POST'])
+def crear_usuario():
+    
+    email=request.json.get("email")
+    first_name=request.json.get("first_name")
+    last_name=request.json.get("last_name")
+    password=request.json.get("password")
+    newUser=User(email=email, first_name=first_name, last_name=last_name, password=password, is_active= True )
+    db.session.add(newUser)
+    db.session.commit()
+    response_body = {
+        "message": "usuario creado exitosamente",
+        "first_name":newUser.first_name,
+        "last_name":newUser.last_name,
+        "id":newUser.id
+    }
+    return jsonify(response_body), 201
+
+#ruta para obtener los usuarios
+@app.route('/user', methods=['GET'])
+def listar_usuarios():
+    usuarios = User.query.all()
+    usuarios = list(map(lambda user: user.serialize(), usuarios ))
+    
+    response_body = {
+        "message": "Hello, this is your GET /user response ",
+        "lista_usuarios": usuarios
+    }
+
+    return jsonify(response_body), 200
+
+#ruta para agregar a una persona a favoritos base Favorites_People()
+@app.route('/favorites/people', methods=['POST'])
+def crear_personaje():
+
+    user_id=request.json.get("user_id")
+    name=request.json.get("name")
+    picture_url=request.json.get("picture_url")
+    description=request.json.get("description")
+    newPeople=People_Favorites(user_id=user_id, name=name, picture_url=picture_url, description=description)
+    db.session.add(newPeople)
+    db.session.commit()
+
+    response_body = {
+        "message": "Personaje creado exitosamente ",
+        "name": newPeople.name
+    }
+    return jsonify(response_body), 200
+
+#ruta para obtener la lista de personajes favoritos de un usuario filtrar por usuario id actual
+@app.route('/favorites/people', methods=['GET'])
+def listar_personajes():
+    personajes = People_Favorites.query.all()
+    personajes = list(map(lambda people: people.serialize(), personajes ))
+    
+    response_body = {
+        "message": "Hello, this is your GET /favorites/people response ",
+        "lista_usuarios": personajes
+    }
+
+    return jsonify(response_body), 200
+
+#ruta para obtener detalle de un personaje de los favoritos
+@app.route('/favorites/people/<int>', methods=['GET'])
+def listar_people2():
+     response_body = {
+        "msg": "Hello, this is your GET /people response "
+    }
+
+#ruta agregar un plalneta al listado de los favoritos base Favorites_Planets()
+@app.route('/favorites/planet', methods=['POST'])
+def create_planet():
+
+    user_id=request.json.get("user_id")
+    planetas_name=request.json.get("planetas_name")
+    planetas_picture_url=request.json.get("planetas_picture_url")
+    planetas_description=request.json.get("planetas_description")
+    
+    newPlanet=Planets_Favorites(user_id=user_id, planetas_name=planetas_name, planetas_picture_url=planetas_picture_url, planetas_description=planetas_description)
+    db.session.add(newPlanet)
+    db.session.commit()
+    response_body = {
+        "message": "planeta creado exitosamente",
+        "name":newPlanet.planetas_name
+        
+    }
+    return jsonify(response_body), 201
+
+#ruta para obtener listado de planetas favoritos de un usuario filtrar por usuario id actual
+@app.route('/favorites/planet', methods=['GET'])
+def listar_planetas():    
+    planetas = Planets_Favorites.query.all()
+    planetas = list(map(lambda planetas: planetas.serialize(), planetas ))
+    
+    response_body = {
+        "message": "Hello, this is your GET /favorites/planets response ",
+        "lista_usuarios": planetas
+    }
+
+    return jsonify(response_body), 200
+
+#ruta para obtener el listado de planetas favoritos de un usuario
+@app.route('/favorites/planet/<int>', methods=['GET'])
+def listar_planetas1():    
+    response_body = {
+        "msg": "Hello, this is your GET /people response "
+    }
+
+
+#ruta para obtener los planetas y personajes favoritos de un usuario
+@app.route('/favorites', methods=['GET'])
+def listar_Favoritos():    
+    planetas = Planets_Favorites.query.all()
+    planetas = list(map(lambda planetas: planetas.serialize(), planetas ))
+    personajes = People_Favorites.query.all()
+    personajes = list(map(lambda people: people.serialize(), personajes ))
+    
+    response_body = {
+        "message": "Hello, this is your GET /favorites response ",
+        "lista_planetas":planetas,
+        "lista_personajes": personajes
+    }
     return jsonify(response_body), 200
